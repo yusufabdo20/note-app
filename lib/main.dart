79c55fca98d29582931996f1f68a61a136a6eb55
yusufabdo20 <yusufa.fattah@gmail.com>
2023-11-29 16:23:26 +0200

    add custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note/views/notes_screen.dart';
 
 void main() {
@@ -9,11 +10,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(
-        useMaterial3: true,
-      ),
-      home: const NotesScreen(),
-    );
+    return ScreenUtilInit(
+        designSize: Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData.dark(
+              useMaterial3: true,
+            ),
+            home: const NotesScreen(),
+          );
+        });
   }
 }
--- lib/views/notes_screen.dart
+++ lib/views/notes_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: NotesViewBody()),
+    );
+  }
+}
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 25.sp,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(5),
+          ),
+          child: const Icon(
+            Icons.search,
+          ),
+        ),
+      ],
+    );
   }
 }
